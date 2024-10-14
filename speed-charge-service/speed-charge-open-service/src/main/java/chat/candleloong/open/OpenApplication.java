ee87087b85574cf64fa48bd96ebb7f32d154b6db
siberia.hu <wisechatteahouse@gmail.com>
2024-10-14 13:57:22 +0800

    feat:新增open服务（走互联互通协议）

---
+++ speed-charge-service/speed-charge-open-service/src/main/java/chat/candleloong/open/OpenApplication.java
@@ -0,0 +1,11 @@
+package chat.candleloong.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApplication.class, args);
+    }
+}
