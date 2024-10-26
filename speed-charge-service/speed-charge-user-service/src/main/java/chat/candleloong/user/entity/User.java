ece5289f4ac59473094076161a992fe0e18d4319
siberia.hu <WiseChatTeahouse@gmail.com>
2024-10-26 22:14:33 +0800

    feat:用户实体类

---
+++ speed-charge-service/speed-charge-user-service/src/main/java/chat/candleloong/user/entity/User.java
@@ -0,0 +1,13 @@
+package chat.candleloong.user.entity;
+
+import lombok.Data;
+
+/**
+ * @Author siberia.hu
+ * @Package chat.candleloong.user.entity
+ * @Date 2024/10/26 22:13
+ */
+@Data
+public class User {
+    private Long id;
+}
